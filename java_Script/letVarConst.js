58614e603db1bf273fae835da3ade274e2351054
chinmaya-dash <149455974+chinmaya-dash@users.noreply.github.com>
2024-07-24 15:35:16 +0530

    updated

---
+++ java_Script/letVarConst.js
@@ -0,0 +1,6 @@
+const AccountId = 2101287283;
+let Email = "chinmayadash442004@gmail.com";
+var AccountPassword = "123456789"
+AccountIdCity = "jajpur";
+
+console.table([AccountId,Email,AccountPassword,AccountIdCity]);
