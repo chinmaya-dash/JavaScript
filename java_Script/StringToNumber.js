4f7dc936c11287e71f514d2e2a6d5833151145de
chinmaya-dash <chinmayadash442004@gmail.com>
2024-07-25 19:06:43 +0530

    String To Number

---
+++ java_Script/StringToNumber.js
@@ -0,0 +1,13 @@
+let number = 33
+
+let stringNumber = String(number)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+let value = 3;
+// let negetiveValue = -value;
+// console.log(negetiveValue);
+// console.log(typeof(negetiveValue));
+
+// console.log(typeof(1 +5+6503256
+// console.log(counter++);
+// console.log("3">2);
